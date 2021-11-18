554e9ab717b80647f9d17c1cb72cc92fbfcbcc9c
JPEtienne <ji.pe.7@hotmail.com>
2021-11-18 14:32:47 +0100

    Animation scene

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,4 +1,4 @@
-import React, { Suspense, useMemo } from 'react'
+import React, { Suspense, useMemo, useCallback, useRef } from 'react'
 import {
   Canvas,
   extend,
@@ -11,9 +11,21 @@ import circleImg from '../assets/circle.png'
 
 function Points() {
   const imgTexture = useLoader(THREE.TextureLoader, circleImg)
+  const bufferRef = useRef()
 
-  const count = 100
-  const separation = 3
+  let t = 0
+  let f = 0.8
+  let a = 0.5
+
+  const graph = useCallback(
+    (x, y) => {
+      return Math.sin(f * (x * 0.7 + y * 0.3 + t)) * a
+    },
+    [t, f, a]
+  )
+
+  const count = 30
+  const separation = 0.5
   let positions = useMemo(() => {
     let positions = []
 
@@ -21,18 +33,34 @@ function Points() {
       for (let zi = 0; zi < count; zi++) {
         let x = separation * (xi - count / 2)
         let z = separation * (zi - count / 2)
-        let y = 0
+        let y = graph(x, z)
         positions.push(x, y, z)
       }
     }
 
     return new Float32Array(positions)
-  }, [count, separation])
+  }, [count, separation, graph])
+
+  useFrame(() => {
+    t += 0.01
+    const positions = bufferRef.current.array
+    let i = 0
+    for (let xi = 0; xi < count; xi++) {
+      for (let zi = 0; zi < count; zi++) {
+        let x = separation * (xi - count / 2)
+        let z = separation * (zi - count / 2)
+        positions[i + 1] = graph(x, z)
+        i += 3
+      }
+    }
+    bufferRef.current.needsUpdate = true
+  })
 
   return (
     <points>
       <bufferGeometry attach="geometry">
         <bufferAttribute
+          ref={bufferRef}
           attachObject={['attributes', 'position']}
           array={positions}
           count={positions.length / 3}
@@ -56,11 +84,8 @@ function Points() {
 
 export default function AnimationCanvas() {
   return (
-    <Canvas
-      colorManagement={false}
-      camera={{ position: [100, 10, 0], fov: 75 }}
-    >
-        <Points />
+    <Canvas colorManagement={false} camera={{ position: [50, 35, 90], fov: 5 }}>
+      <Points />
       {/* <Suspense fallback={null}>
       </Suspense> */}
     </Canvas>
